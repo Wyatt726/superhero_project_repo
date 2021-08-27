c8292f049bad048164d6fb47e10c292c38e5b0d2
Wyatt726 <wyatt726@aol.com>
2021-08-27 03:06:29 -0400

    adding in url path as well as executable table blocks for hero add ins

--- superhero_project/superheros/urls.py
+++ superhero_project/superheros/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_anme = 'superheros'
 urlpattern = [
     path('', views.index, name='index'),
-    path('<int:hero_id>/'), views.detail, name='detail')
+    path('<int:hero_id>/'), views.detail, name='detail'),
+    path('new/', views.create, name='create')
 
 ]
+
--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
 # Create your views here.
 def index(request):
@@ -12,3 +12,21 @@ def index(request):
 
 def detail(request, hero_id):
     single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheros/details.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name= request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary= request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero=Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheros:index'))
+
+    else:
+        return render(request, 'superheros/create.html')
