91e9ffa3dcfe1c8175acc9e8fbcbf531e73a1845
Wyatt726 <wyatt726@aol.com>
2021-08-27 02:00:16 -0400

    adding in the index requests to index.html, still getting caused by a circular import.

--- superhero_project/superheros/urls.py
+++ superhero_project/superheros/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_anme = 'superheros'
 URLPattern = [
-    path('index/', views.index, name='index')
+    path('', views.index, name='index')
 ]
--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Superhero
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    all_heros = Superhero.objects.all()
+    context = {
+        'all_heros': all_heros
+    }
+    return render(request, 'superheros/index.html', context)
